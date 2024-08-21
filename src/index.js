b04858d84ef09aa14edc97700cb2a6bab9eecceb
lbinz <38222532+lbinz@users.noreply.github.com>
2024-08-21 10:16:05 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.lvshell.com": dockerHub,
+  "quay.lvshell.com": "https://quay.io",
+  "gcr.lvshell.com": "https://gcr.io",
+  "k8s-gcr.lvshell.com": "https://k8s.gcr.io",
+  "k8s.lvshell.com": "https://registry.k8s.io",
+  "ghcr.lvshell.com": "https://ghcr.io",
+  "cloudsmith.lvshell.com": "https://docker.cloudsmith.io",
+  "ecr.lvshell.com": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
